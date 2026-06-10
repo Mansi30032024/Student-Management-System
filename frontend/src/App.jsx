e69b1c930c694da3a02b71487e77adc1e9672fcf
Mansi Arora <mansiarora040m@gmail.com>
2026-06-10 16:18:47 +0530

    Frontend done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,85 +1,89 @@
 import { useState } from "react";
 
-function App(){
+function App() {
   const [students, setStudents] = useState([]);
-   const [count, setCount] = useState(0);
-    const [name, setName] = useState("");
-     const [course, setCourse] = useState("");
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  const [course, setCourse] = useState("");
 
   //function to get Student data from Backend
-  const getStudents = async ()=>{
-    const response = await fetch('http://localhost:8080/students');
+  const getStudents = async () => {
+    const response = await fetch("http://localhost:8080/students");
     const data = await response.json();
     console.log(data);
     setStudents(data);
-  }
+  };
 
-    const getBcaStudents = async ()=>{
-    const response = await fetch('http://localhost:8080/students/bca');
+  const getBcaStudents = async () => {
+    const response = await fetch("http://localhost:8080/students/bca");
     const data = await response.json();
     console.log(data);
     setStudents(data);
-  }
+  };
 
-  
-    const fetchTotalStudentsCount= async ()=>{
-    const response = await fetch('http://localhost:8080/students/count');
+  const fetchTotalStudentsCount = async () => {
+    const response = await fetch("http://localhost:8080/students/count");
     const data = await response.json();
     console.log(data);
     setCount(data);
-  }
+  };
 
-  const addStudent= async ()=>{
-    const response = await fetch('http://localhost:8080/students' , {
+  const addStudent = async () => {
+    const response = await fetch("http://localhost:8080/students", {
       method: "POST",
-      headers:{
-        "Content-Type": "application/json"
+      headers: {
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({name, course})
+      body: JSON.stringify({ name, course }),
     });
-    if(response.ok){
+    if (response.ok) {
       alert("Student registered");
-    }
-    else{
+    } else {
       alert("Failed to reguster!");
     }
-  }
+  };
 
-  return(
+  return (
     <div>
-      <button onClick={getStudents}>
-        Fetch Students
-      </button>
+      <button onClick={getStudents}>Fetch Students</button>
 
-      <button onClick={getBcaStudents}>
-        Show BCA Students
-      </button>
-      
+      <button onClick={getBcaStudents}>Show BCA Students</button>
+
       <ul>
-       {students.map(student=>(
-        <li key={student.id}>
-          {student.name} -  {student.course}
-        </li>
-       ))
-       }
+        {students.map((student) => (
+          <li key={student.id}>
+            {student.name} - {student.course}
+          </li>
+        ))}
       </ul>
 
-      <button onClick={fetchTotalStudentsCount}>Get Total Students Count</button>
+      <button onClick={fetchTotalStudentsCount}>
+        Get Total Students Count
+      </button>
       <p>Total students: {count} </p>
 
-    <h1>Student Registration Form</h1>
-    <input type="text"  placeholder="Name" value={name} onChange={(e)=> setName(e.target.value)}/>
-    <input type="text"  placeholder="Course" value={course} onChange={(e)=> setCourse(e.target.value)}/>
-    
+      <h1>Student Registration Form</h1>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Course"
+        value={course}
+        onChange={(e) => setCourse(e.target.value)}
+      />
+
       <button onClick={addStudent}>Register</button>
-
-
     </div>
-  )
+  );
 }
 export default App;
 
-    {/* 
+{
+  /* 
       <h1>{student?.name}</h1>
       <h1>{student?.id}</h1>
 
@@ -89,4 +93,5 @@ This prevents runtime errors like:
 
 Cannot read properties of undefined (reading 'name')
 The { ... } syntax is JSX’s way of embedding JavaScript expressions inside HTML-like code.
- */}
+ */
+}
