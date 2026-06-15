d3526383509427f7bd6727b0e33513205c819bd1
Mansi Arora <mansiarora040m@gmail.com>
2026-06-15 15:54:12 +0530

    Day 9 Token Generation done

--- backend/sms/src/main/java/com/example/sms/controller/AuthController.java
+++ backend/sms/src/main/java/com/example/sms/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.example.sms.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,16 +8,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.sms.dto.LoginRequestDTO;
+import com.example.sms.service.JwtService;
 
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin(origins="http://localhost:5173")
 public class AuthController {
 
+    @Autowired
+    private JwtService service;
+
     @PostMapping("/login")
     public String login(@RequestBody LoginRequestDTO dto){
         if(dto.getUsername().equals("admin") && dto.getPassword().equals("admin")){
-          return "JWT Token";
+          return  service.generateToken(dto.getUsername());
         }
 
         return "Invalid credentials";
