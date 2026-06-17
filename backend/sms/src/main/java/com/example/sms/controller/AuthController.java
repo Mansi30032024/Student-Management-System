e4eba5aa2306b760ef3fb62086a5a81da02c536b
Mansi Arora <mansiarora040m@gmail.com>
2026-06-17 14:41:18 +0530

    issue fixed

--- backend/sms/src/main/java/com/example/sms/controller/AuthController.java
+++ backend/sms/src/main/java/com/example/sms/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.example.sms.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +28,11 @@ public class AuthController {
         }
 
         return "Invalid credentials";
+    }
+
+    @GetMapping("/google-success")
+    public String success(@AuthenticationPrincipal OAuth2User user) {
+
+        return "Welcome " + user.getAttribute("name");
     }
 }
